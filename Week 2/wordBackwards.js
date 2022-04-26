b5ede1c25dde16c05a200eaec64080a4335f67ab
DomGibson <103586347+DomGibson@users.noreply.github.com>
2022-04-26 09:07:28 +0100

    Add files via upload

---
+++ Week 2/wordBackwards.js
@@ -0,0 +1,11 @@
+// let word = "chocolate"
+
+// let wordBw = word.split("").reverse().join("");
+
+// console.log(wordBw);
+
+
+function reverseString(myThing) {
+    return myThing.split('').reverse().join('')
+}
+console.log(reverseString("Hello World!"));
