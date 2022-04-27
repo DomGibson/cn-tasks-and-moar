ae197a34b1894b45b72271e23aa237e0ec24f8ba
DomGibson <103586347+DomGibson@users.noreply.github.com>
2022-04-27 09:18:41 +0100

    Add files via upload

---
+++ Week 2/dataCheckerTask.js
@@ -0,0 +1,14 @@
+datachecker = (myString, myRank) => {
+
+    myString = "codenation"
+    myRank = 1
+
+    if (myString == "codenation" && myRank == 1) {
+    console.log("True: Template String");
+    } else if (myString != "codenation" && myRank != 1) {
+    console.log("False: Template String")
+    } else (myString != "codenation" && myRank != 1)
+        console.log("What Is Going On Here!")
+
+    return(datachecker)
+}
